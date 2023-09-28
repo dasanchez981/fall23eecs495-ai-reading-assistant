d071b26efcc69a5753be9ac1da9a0b01d0874578
Rishiraj Chandra <rajchan@umich.edu>
2023-09-28 16:05:35 -0400

    Trying to integrate TestingExtension with AI-Reading-Assistant extension

--- ai-reading-assistant/src/App.tsx
+++ ai-reading-assistant/src/App.tsx
@@ -1,6 +1,6 @@
 // import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import { summaryCall } from './components/SummaryCall'
 
@@ -9,15 +9,32 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <div id="sidebar_container">
+            <header>
+                <h1>AI-Powered Reading Assistant (AIRA)</h1>
+            </header>
+            <br></br>
+            <div>
+                <h4>Please insert text into the text block below:</h4>
+                <div id="input_with_buttons">
+                    {/* <!-- On button click, call the API necessary to do the function, pull element value to upload the text through JS--> */}
+                    <button type="button" id="textToSpeech" className="inputButtons">Text to Speech</button>
+                    <button type="button" id="summarization" className="inputButtons">Summarize</button>
+                    <textarea id="manual_input" name="manual_input" placeholder="Enter your text to Speak/Summarize..."></textarea>
+                    {/* <button type="button" id="clearTextButton" onClick="document.getElementById('manual_input').value = ''">Clear Text</button> */}
+                </div>
+                
+            </div>
+
+            <br></br>
+
+            <div>
+                <h4>The resulting output will be displayed below:</h4>
+                <textarea id="manual_output" name="manual_output" placeholder="Result from AI summarization..."></textarea>
+            </div>
+
+           
+        </div>
       <div className="card">
         {/* <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
