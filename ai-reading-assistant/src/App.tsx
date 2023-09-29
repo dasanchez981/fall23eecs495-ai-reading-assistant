b6273c2429e627f8237cdb7edcc9eaf20333ffdc
Rishiraj Chandra <rajchan@umich.edu>
2023-09-29 11:53:50 -0400

    Eliminated extra button, summarize button now functional

--- ai-reading-assistant/src/App.tsx
+++ ai-reading-assistant/src/App.tsx
@@ -19,7 +19,9 @@ function App() {
                 <div id="input_with_buttons">
                     {/* <!-- On button click, call the API necessary to do the function, pull element value to upload the text through JS--> */}
                     <button type="button" id="textToSpeech" className="inputButtons">Text to Speech</button>
-                    <button type="button" id="summarization" className="inputButtons">Summarize</button>
+                    <button onClick={() => summaryCall()} type="button" id="summarization" className="inputButtons">
+                      Summarize 
+                    </button>
                     <textarea id="manual_input" name="manual_input" placeholder="Enter your text to Speak/Summarize..."></textarea>
                     {/* <button type="button" id="clearTextButton" onClick="document.getElementById('manual_input').value = ''">Clear Text</button> */}
                 </div>
@@ -32,23 +34,7 @@ function App() {
                 <h4>The resulting output will be displayed below:</h4>
                 <textarea id="manual_output" name="manual_output" placeholder="Result from AI summarization..."></textarea>
             </div>
-
-           
-        </div>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <button onClick={() => summaryCall()}>
-          Generate Summary
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on either logo to learn more
-      </p>
+        </div>   
     </>
   )
 }
