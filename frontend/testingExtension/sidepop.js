d0da0928555b00af99c67458119e5041f073a932
n-curdo <112668807+n-curdo@users.noreply.github.com>
2023-10-09 13:16:27 -0400

    Add files via upload
    Altered UI popup to appear on the page as a sidebar

--- frontend/testingExtension/sidepop.js
+++ frontend/testingExtension/sidepop.js
@@ -1,15 +1,18 @@
-let div = document.createElement("div");
-div.id = 'toolbar';
-document.body.appendChild(div);
 
-fetch(chrome.runtime.getURL('index.html'))
-    .then((response) => response.text())
-    .then((data) =>{
-        document.getElementById('toolbar').innerHTML = data;
-        let css = chrome.runtime.getURL('style.css');
-        let link = document.createElement("link");
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        link.href = css;
-        document.head.appendChild(link);
-    });
+if(!(document.getElementsByTagName("html")[0].style.marginLeft == "380px")){
+    let originHTML = document.getElementsByTagName("html")[0].style.marginLeft = "380px";
+    let iframe = document.createElement("iframe");
+    iframe.id = 'extensionSidebar';
+    document.body.appendChild(iframe);
+
+    let extURL = chrome.runtime.getURL('index.html');
+    iframe.src=extURL;
+    iframe.scrolling="no";
+
+    iframe.style="position: fixed; top: 0; height: 100%; left: 0; width: 354px; padding:10px 13px; background-color:#D3D9DE; z-index: 99999; margin: 0;";
+}else{
+    document.getElementsByTagName("html")[0].style.marginLeft = "0px";
+    document.getElementById('extensionSidebar').remove();
+}
+
+
