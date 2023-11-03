f50cb2ef8ae39d7f810cb473c03714edfbd5ed61
Rishiraj Chandra <rajchan@umich.edu>
2023-11-03 15:55:40 -0400

    Implemented context menu items, items not yet functional but extension in good state

---
+++ ai-reading-assistant/public/service-worker.js
@@ -0,0 +1,59 @@
+// This script runs in the background as soon as the extension is installed
+// All the logs can be found by navigating to chrome://extensions and clicking on service worker
+
+// This adds custom menu items to user's right click menu
+function setupContextMenu() {
+    console.log("Added context items to menu")
+    chrome.contextMenus.create({
+      id: 'summarize-text',
+      title: '🍆 Summarize 📚📝',
+      contexts: ['selection']
+    });
+    chrome.contextMenus.create({
+        id: 'text-to-speech',
+        title: '🍆 Speak 🗣️💬🔊',
+        contexts: ['selection']
+    });
+    chrome.contextMenus.create({
+        id: 'text-focus',
+        title: '🍆 Text Focus 👀🔍',
+        contexts: ['selection']
+    });
+  }
+  
+  // Extension needs to be refreshed to activate any changes here
+  chrome.runtime.onInstalled.addListener(() => {
+    console.log("Installed extension")
+    setupContextMenu();
+  });
+
+  // Triggers whenever a context menu item is clicked, sends data to App.tsx for functionality
+  chrome.contextMenus.onClicked.addListener((data) => {
+    console.log("Clicked on context menu item")
+
+    if (data.menuItemId === 'summarize-text') {
+      console.log("Sending message to summarize to App.tsx!")
+      chrome.runtime.sendMessage({
+          name: 'summarize-text',
+          data: { value: data.selectionText }
+      });
+    }
+    else if (data.menuItemId === 'text-to-speech') {
+        console.log("Sending message to speak to App.tsx!")
+        chrome.runtime.sendMessage({
+            name: 'text-to-speech',
+            data: { value: data.selectionText }
+        });
+    }
+    else if (data.menuItemId === 'text-focus') {
+        console.log("Sending message to focus text to App.tsx!")
+        chrome.runtime.sendMessage({
+            name: 'text-focus',
+            data: { value: data.selectionText }
+        });
+    }
+  });
+
+  // TODO: Issue-> context menu items appear once installed even when side panel closed
+  // Solution: Catch this condition (i.e. when user clicks on summarize) 
+  // and alert user with popup to open side panel for context menu functionality
--- ai-reading-assistant/src/App.tsx
+++ ai-reading-assistant/src/App.tsx
@@ -5,7 +5,9 @@ import { speakText } from './components/SpeakText'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import Button from 'react-bootstrap/Button';
+// import Popover from "react-text-selection-popover";
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import Form from 'react-bootstrap/Form';
 
 function App() {
   const [text, setText] = useState("")
@@ -15,16 +17,22 @@ function App() {
 
   chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
   
-  // Chrome background listener
-  chrome.runtime.onMessage.addListener((message) => {
-    if (message.action === 'setText') {
-      // Set the text and speechText values here
-      console.log("Received message from Chrome!")
-      setText(message.text);
-      // setSpeechText(message.text);
+   // Chrome background listener to receive messages from context menu items in service-worker.js
+   chrome.runtime.onMessage.addListener(({ name, data }) => {
+    if (name === 'summarize-text') {
+      console.log("Received message to summarize from service-worker.js!");
+      console.log(data)
+    }
+    else if (name === 'text-to-speech') {
+      console.log("Received message to speak from service-worker.js!")
+      console.log(data)
+    }
+    else if (name === 'text-focus') {
+      console.log("Received message to focus text from service-worker.js!")
+      console.log(data)
     }
   });
-
+  
   // Function to handle summaries
   const handleSumSubmit = (event: any) => {
     event.preventDefault();
@@ -75,93 +83,161 @@ function App() {
     }
   };
 
+  // This is experimental approach to implementing toolbar functionality
+  // Right now the 'Activate Toolbar' button has to be pressed in order for the toolbar 
+  // listener to be activated
   const onclick = async () => {
     let [tab] = await chrome.tabs.query({active: true})
     if (tab.url?.startsWith("chrome://")) return undefined;
+    console.log("Toolbar has been activated")
     // This can't access React variables so need to send through Chrome API
     chrome.scripting.executeScript({
       target: { tabId: tab.id! },
       func: () => {
-        const highlighted = window.getSelection()?.toString()
-        if (highlighted != null) {
-          // alert(highlighted)
-          // Send the highlighted text to the background script
-          chrome.runtime.sendMessage({ action: 'setText', text: highlighted });
-        }
+        console.log("Executing script on webpage to add listener")
+        document.addEventListener("click", () => {
+          const selection = window.getSelection()
+          const highlighted = window.getSelection()?.toString()
+          console.log("Within toolbar listener")
+          //console.log(highlighted);
+          if(highlighted != '') {
+            console.log("If shown then activate toolbar works!")
+            console.log("Selection location: ");
+            console.log(selection?.getRangeAt(0))
+            console.log("bounding rectangle:")
+            console.log(selection?.getRangeAt(0).getBoundingClientRect())
+            
+            
+            // Both methods may need to extend an HTMLElement and then appended like in iFrame method
+            // Extending HTML element is involved and requires a lot of code and coordination
+            // In consideration but need to think about, see https://github.com/MariusBongarts/medium-highlighter
+            // for example of what we might have to do
+            
+            // *** Popover Method***
+            // <Popover>
+            //   <div className="popover">
+            //       <b>B</b>
+            //   </div>
+            // </Popover>
+            
+            // *** Manual IFrame Method ***
+            // var rect = selection?.getRangeAt(0).getBoundingClientRect();
+            // var elem = document.createElement("iframe");
+            // document.body.appendChild(elem);
+            // elem.setAttribute("style", "position: fixed; height: 50px; width: 50px; padding:10px 13px; background-color:#D3D9DE; z-index: 99999; margin: 0;");
+            // let element = document.createElement("iframe");
+            // document.body.appendChild(element);
+            // element.setAttribute("style", "position: fixed; top: 0; height: 50px; left: 0; width: 50px; padding:10px 13px; background-color:#D3D9DE; z-index: 99999; margin: 0;");
+            // elem.setAttribute("style",`top: ${rect?.top}`)
+            // elem.setAttribute("style",`left: ${rect?.left}`)
+            //https://stackoverflow.com/questions/1589721/get-selected-text-position-and-place-an-element-next-to-it/1589912#1589912
+            /*var range = selection?.getRangeAt(0).cloneRange()
+            range?.collapse(false)
+            var markerEl = document.createElement("span");
+            markerEl.appendChild( document.createTextNode("\ufeff") );
+            range?.insertNode(markerEl);
+
+            if (markerEl) {
+              // Lazily create element to be placed next to the selection
+              var selectionEl;
+              if (!selectionEl) {
+                  selectionEl = document.createElement("div");
+                  selectionEl.style.border = "solid darkblue 1px";
+                  selectionEl.style.backgroundColor = "lightgoldenrodyellow";
+                  selectionEl.innerHTML = "&lt;- selection";
+                  selectionEl.style.position = "absolute";
+  
+                  document.body.appendChild(selectionEl);
+              }
+              var obj = markerEl;
+              var left = 0, top = 0;
+              do {
+                  left += obj.offsetLeft;
+                  top += obj.offsetTop;
+              } while (obj = obj.offsetParent);
+
+                  // Move the button into place.
+                  // Substitute your jQuery stuff in here
+                  selectionEl.style.left = left + "px";
+                  selectionEl.style.top = top + "px";
+
+                  markerEl.parentNode?.removeChild(markerEl);
+            }*/
+          }
+        });
       }
     });
   }
-
-  // console.log("Text to be analyzed")
-  // console.log(text)
-  // console.log("The speech URL is set below:")
-  // console.log(speechURL)
-  // console.log("Value of audio type: ")
-  // console.log(audioType)
     
   return (
     <>
       <div id="sidebar_container">
-            <header>
-                <h2>AI-Powered Reading Assistant (AIRA)</h2>
-            </header>
-            <br></br>
-            <Button onClick={onclick} variant="warning" id="highlight">Transfer Highlighted Text</Button>
-            <br></br>
-            <p id="audioIndicator"> {audioType} </p>
-            <div id="audio_container">
-              <AudioPlayer 
-                autoPlay
-                src={speechURL}
-                showSkipControls={false}
-                showJumpControls={true}
-                showFilledProgress={true}
-                showFilledVolume={false}
-                hasDefaultKeyBindings={false}
-                autoPlayAfterSrcChange={false}
-                style={{
-                  backgroundColor: 'white',
-                  border: '1px solid #ccc',
-                  padding: '10px',
-                  width: '300px',
-                  borderRadius: '5px',
-                }}
+        <header>
+          <h2>AI-Powered Reading Assistant (AIRA)</h2>
+        </header>
+        <br></br>
+        {/* <Form>
+              <Form.Check // prettier-ignore
+                type="switch"
+                id="toolbar-mode"
+                label="Enable toolbar"
               />
-            </div>
-            <div id="textToSynth">  
+            </Form> */}
+        <Button onClick={onclick} variant="warning" id="highlight">
+          Activate Toolbar (Deprecated)
+        </Button>
+        <br></br>
+        <p id="audioIndicator"> {audioType} </p>
+        {/* TODO: Figure out why audio playback being goofy */}
+        <div id="audio_container">
+          <AudioPlayer
+            autoPlay
+            src={speechURL}
+            showSkipControls={false}
+            showJumpControls={true}
+            showFilledProgress={true}
+            showFilledVolume={false}
+            hasDefaultKeyBindings={false}
+            autoPlayAfterSrcChange={false}
+            style={{
+              backgroundColor: "white",
+              border: "1px solid #ccc",
+              padding: "10px",
+              width: "300px",
+              borderRadius: "5px",
+            }}
+          />
+        </div>
+        <div id="textToSynth">
+          <form onSubmit={onSubmit}>
+            <input type="submit" value="Speak Text" id="speakbutton" />
+            <input type="submit" value="Summarize" id="sumbutton" />
 
-              <form onSubmit={onSubmit}>
-                      <input type="submit" value="Speak Text" id="speakbutton"/>
-                      <input type="submit" value="Summarize" id="sumbutton"/>
-
-                      <textarea 
-                        id="manual_input" 
-                        name="manual_input" 
-                        placeholder="Enter your text to Speak/Summarize..."
-                        value={text}
-                        onChange={(e) => setText(e.target.value)}
-                      >
-                      </textarea>
-              </form>
-            </div>
-            <div>
-              <br></br>
-              <h5>AI-Generated Summary:</h5>
-              <form onSubmit={onSubmit}>
-                <input type="submit" value="Speak Summary" id="speakSumbutton"/>
-                <textarea 
-                  id="manual_output" 
-                  name="manual_output" 
-                  placeholder="Result from AI summarization..."
-                  value={response}
-                  >
-                </textarea>
-              </form>
-            </div>
-            
-        </div>   
+            <textarea
+              id="manual_input"
+              name="manual_input"
+              placeholder="Enter your text to Speak/Summarize..."
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+            ></textarea>
+          </form>
+        </div>
+        <div>
+          <br></br>
+          <h5>AI-Generated Summary:</h5>
+          <form onSubmit={onSubmit}>
+            <input type="submit" value="Speak Summary" id="speakSumbutton" />
+            <textarea
+              id="manual_output"
+              name="manual_output"
+              placeholder="Result from AI summarization..."
+              value={response}
+            ></textarea>
+          </form>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
